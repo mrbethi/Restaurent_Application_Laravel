dab8dea5f0458cd85e25772d9db751e6339d4d81
mrbethi <murshidarahman1213@gmail.com>
2022-01-21 15:22:32 +0600

    table-reservation-done

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,13 @@ Route::get("/deleteuser/{id}",[AdminController::class,"deleteuser"]);
 
 Route::get("/redirects",[HomeController::class,"redirects"]);
 
+
+
+Route::post("/reservation",[AdminController::class,"reservation"]);
+
+
+Route::get("/viewreservation",[AdminController::class,"viewreservation"]);
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
